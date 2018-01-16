070e1f14cbbfa65a6907a2793676c88492f3d6ad
KiranB <kiran.bangale@derivco.co.za>
2018-01-16 18:09:33 +0530

    Updates on loaders

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ https://scotch.io/tutorials/setting-up-webpack-for-any-project?utm_source=newsle
 <!-- Initial steps -->
 npm install -g yarn
 yarn init
+yarn <!-- for insatalling all packages from package.json -->
 yarn add webpack webpack-dev-server html-webpack-plugin -webpack-dev-server 
 
 
@@ -21,11 +22,19 @@ yarn
 
 Webpack: 
 html-webpack-plugin: Includes js bundle into body element of given template
+babel-core: ES6+ transpiler
+babel-loader: Webpack loader plugin for babel
+babel-preset-env: preset that compiles ES6+ down to ES5 by automatically determining the Babel plugins and polyfills you need based on your targeted browser or runtime environments
+style-loader: Webpack loader plugin for loading styles
+css-loader: Webpack loader plugin for css
+less-loader: Webpack loader plugin for less
+extract-text-webpack-plugin: Extracts CSS from JS bundle, or bundles, into a separate CSS file
+
 
 Sublime: 
 HTML Snippets: Sublime package for Html code snippets
 prettier: JS code formatter
-
+javascript snippets: Sublime package for Js code snippets
 
 
 <!-- package.json -->
@@ -39,3 +48,5 @@ prettier: JS code formatter
 
 <!-- Notes -->
 - Press F6 in sublime for spell checks
+- Loaders do the pre-processing transformation of any file format when you use them; they work at the individual file level during or before the bundle is generated. On the other hand, plugins are quite simple since they expose only one single function to webpack and are not able to influence the actual build process.
+- Plugins work at bundle or chunk level and usually work at the end of the bundle generation process.
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,48 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-	
-	entry: __dirname + '/src/app/index.js', // Webpack entry point. Module to start building dependency graph
-	
-	output: {
-		path: __dirname + '/dist', // Folder to store generated bundle
-		filename: 'bundle.js',  // Name of generated bundle after build
-		publicPath: '/' // Public URL of the output directory when referenced in a browser
-	},
-	
-	module: { // Define file patterns and their loaders
-		rules: [
 
-		]
-	},
-	
-	plugins: [ // Plugins array to apply to build chunk
-		new HtmlWebpackPlugin({
-			template: __dirname + '/src/public/index.html',
-			inject: 'body'
-		})
-	],
-	
-	devServer: { // Config of webpack-dev-server
-		contentBase: './src/public', // Src of static assets
-		port: 7700 // Port to run dev server
-	}
+    entry: __dirname + '/src/app/index.js', // Webpack entry point. Module to start building dependency graph
+
+    output: {
+        path: __dirname + '/dist', // Folder to store generated bundle
+        filename: 'bundle.js', // Name of generated bundle after build
+        publicPath: '/' // Public URL of the output directory when referenced in a browser
+    },
+
+    module: { // Define file patterns and their loaders
+        rules: [{
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: [
+                    /node_modules/
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader' },
+                        { loader: 'sass-loader' }
+                    ],
+                })
+            },
+        ]
+    },
+
+    plugins: [ // Plugins array to apply to build chunk
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/public/index.html',
+            inject: 'body'
+        }),
+        new ExtractTextPlugin('styles.css')
+    ],
+
+    devServer: { // Config of webpack-dev-server
+        contentBase: './src/public', // Src of static assets
+        port: 7700 // Port to run dev server
+    }
 
 };
